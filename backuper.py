cb67ba97794a240b4f292197c50ddb906ca1780a
stkudryashov <aleks59niki30@gmail.com>
2021-03-24 18:42:24 +0300

    feat upload photos

--- backuper.py
+++ backuper.py
@@ -1,5 +1,5 @@
 import requests
-from pprint import pprint
+import datetime
 
 
 class VkBackup:
@@ -13,9 +13,9 @@ class VkBackup:
             'v': self.version
         }
 
-    def get_photos(self, album):
+    def get_photos(self, vk_album):
         photos_url = self.url + 'photos.get'
-        photos_params = {'album_id': album, 'extended': 1, 'photo_sizes': 1}
+        photos_params = {'album_id': vk_album, 'extended': 1, 'photo_sizes': 1}
         response = requests.get(photos_url, params={**self.params, **photos_params})
         return response.json()['response']['items']
 
@@ -32,15 +32,44 @@ class YaUploader:
             'Authorization': 'OAuth {}'.format(self.token)
         }
 
+    def create_folder(self, yandex_path: str):
+        create_url = self.url + 'resources/'
+        create_params = {'path': yandex_path}
+        response = requests.put(create_url, headers=self.get_headers(), params=create_params)
+        if response.status_code == 201:
+            print(f'Папка {yandex_path} успешно создана')
+        return response.status_code
+
     def upload_from_url(self, yandex_path: str, photo_url: str):
         upload_url = self.url + 'resources/upload'
         upload_params = {'path': yandex_path, 'url': photo_url}
         response = requests.post(upload_url, headers=self.get_headers(), params=upload_params)
-        return response
+        if response.status_code == 202:
+            print(f'Фотография {yandex_path} загружается')
+        return response.status_code
+
+
+def create_backup_yandex(name: str, uploader: YaUploader, backuper: VkBackup, albums):
+    uploader.create_folder('VkBackup/')
+    backup_folder = f'VkBackup/{name}/'
+    uploader.create_folder(backup_folder)
+
+    for album in albums:
+        uploader.create_folder(backup_folder + album)
+        photos = backuper.get_photos(album.lower())
+
+        for photo in photos:
+            photo_path = f'VkBackup/{name}/{album}/' + str(photo['likes']['count']) + '.jpg'
+            uploader.upload_from_url(photo_path, photo['sizes'][-1]['url'])
 
 
 if __name__ == '__main__':
-    uploader = YaUploader(yandex)
-    vk_backup = VkBackup(vk, '5.130')
-    photos = vk_backup.get_photos('profile')
-    uploader.upload_from_url('VK', photos[0]['sizes'][-1]['url'])
+    ya_uploader = YaUploader(yandex)
+    vk_backuper = VkBackup(vk, '5.130')
+
+    albums_list = ['Profile', 'Wall']
+
+    backup_name = datetime.datetime.isoformat(datetime.datetime.now(), sep='-')
+    backup_name = backup_name.replace(':', '-').replace('.', '-')
+
+    create_backup_yandex(backup_name, ya_uploader, vk_backuper, albums_list)
